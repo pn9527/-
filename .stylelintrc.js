d3c3224e65ad3f853961cceee86ec4887cf17f32
pn9527 <625594474@qq.com>
2022-06-23 09:04:38 +0800

    feat: stylelintConfig
    Signed-off-by: pn9527 <625594474@qq.com>

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,25 @@
 module.exports = {
-  extends: 'stylelint-config-standard',
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-css-modules',
+    'stylelint-config-prettier',
+    'stylelint-config-html/vue',
+    'stylelint-config-recommended-vue',
+  ],
+  plugins: [
+    'stylelint-order',
+    'stylelint-declaration-block-no-ignored-properties',
+  ],
+  rules: {
+    indentation: 2,
+    'no-descending-specificity': null, // 禁止在具有较高优先级的选择器后出现被其覆盖的较低优先级的选择器
+    'function-url-quotes': 'always',
+    'selector-attribute-quotes': 'always',
+    'font-family-no-missing-generic-family-keyword': null, // iconfont
+    'plugin/declaration-block-no-ignored-properties': true,
+    'unit-no-unknown': [true, { ignoreUnits: ['rpx'] }],
+    'selector-type-no-unknown': null,
+    'value-keyword-case': ['lower', { ignoreProperties: ['composes'] }],
+  },
+  ignorePath: '.stylelintignore',
 };
